98b91befe09ae46ad2f84fd6844b87529ba6ebea
SiddheshKukade <littlelord124@gmail.com>
2022-04-09 11:00:31 +0530

    📦 NEW: Header Center Done

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,10 +4,11 @@ import Image from 'next/image';
 import {
     BellIcon, ChatIcon, HomeIcon, ChevronDownIcon, UserGroupIcon, ViewGridIcon
 } from "@heroicons/react/solid"
-import { FlagIcon, PlayIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/solid';
+import { FlagIcon, PlayIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline';
+import HeaderIcon from './HeaderIcon';
 const Header = () => {
     return (
-        <div >
+        <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md'>
             {/* Left   -facebook icon and a searcbar */}
             <div className="flex items-center">
                 <Image src="https://links.papareact.com/5me" width={40} height={40} layout='fixed' />
@@ -16,11 +17,15 @@ const Header = () => {
                     <input className='flex ml-3  items-center bg-transparent outline-none' type="text" placeholder='Search Facebook' />
                 </div>
             </div>
-<div>
-    <div className='flex items-center flex-grow'>
-<HeaderIcon Icon={HomeIcon}></HeaderIcon>
-    </div>
-</div>
+            <div className='flex justify-center flex-grow   '>
+                <div className='flex items-center space-x-6 md:space-x-2 '>
+                    <HeaderIcon Icon={HomeIcon} />
+                    <HeaderIcon Icon={FlagIcon} />
+                    <HeaderIcon active={true} Icon={PlayIcon} />
+                    <HeaderIcon Icon={ShoppingCartIcon} />
+                    <HeaderIcon Icon={UserGroupIcon} />
+                </div>
+            </div>
             {/* center */}
             {/* right */}
 
@@ -28,4 +33,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default Header   
